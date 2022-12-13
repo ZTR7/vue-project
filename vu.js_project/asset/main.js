dc2236d461f62ec6eda5c745ce3a88a9e961bd10
ZTR7 <77837949+ZTR7@users.noreply.github.com>
2022-12-13 21:24:17 +0100

    Add files via upload

---
+++ vu.js_project/asset/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data: function(){
+        return{
+            cart:[],
+            premium: true,
+        }
+    },
+    methods:{
+        updateCart(id){
+            this.cart.push(id)
+        }
+    }
+})
